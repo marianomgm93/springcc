a2cb9d7ffcc5926c8bd0441d40d29dd280ef5489
Mariano Gonzalez <marianomgm93@gmail.com>
2024-09-16 13:47:22 -0300

    th_sec

--- ControlClientes/src/main/java/com/example/App/web/ControladorInicio.java
+++ ControlClientes/src/main/java/com/example/App/web/ControladorInicio.java
@@ -4,8 +4,11 @@ import com.example.App.dao.PersonaDao;
 import com.example.App.domain.Persona;
 import com.example.App.service.PersonaService;
 import jakarta.validation.Valid;
+import java.security.Principal;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -16,39 +19,54 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Slf4j
 public class ControladorInicio {
 
-    
     @Autowired
     private PersonaService personaService;
 
     @GetMapping("/")
-    public String inicio(Model model) {
+    public String inicio(Model model, Principal principal) {
         var personas = personaService.listarPersonas();
+        log.info("Ejecutando el controlador de Spring MVC");
+        log.info("Usuario que hizo login: "+principal.getName());
         model.addAttribute("personas", personas);
         return "index";
 
     }
+
     @GetMapping("/agregar")
-    public String agregar(Persona persona){
+    public String agregar(Persona persona) {
         return "modificar";
     }
-    
+
     @PostMapping("/guardar")
-    public String guardar(@Valid Persona persona, Errors errores){
-        if(errores.hasErrors()){
+    public String guardar(@Valid Persona persona, Errors errores) {
+        if (errores.hasErrors()) {
             return "modificar";
         }
         personaService.guardar(persona);
         return "redirect:/";
     }
+
     @GetMapping("/editar/{idPersona}")
-    public String editar(Persona persona,Model model){
-        persona=personaService.encontrarPersona(persona);
+    public String editar(Persona persona, Model model) {
+        persona = personaService.encontrarPersona(persona);
         model.addAttribute("persona", persona);
         return "modificar";
     }
+
     @GetMapping("/eliminar")
-    public String eliminar(Persona persona){
+    public String eliminar(Persona persona) {
         personaService.eliminar(persona);
         return "redirect:/";
     }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login"; // Nombre de la plantilla de Thymeleaf (login.html)
+    }
+
+    @GetMapping("/errores/403")
+    public String accessDenied() {
+        return "errores/403"; // Nombre de la plantilla de Thymeleaf (403.html)
+    }
+
 }
